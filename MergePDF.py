97450339c6136da3a2261797c152b486b05b0c42
GYR <1536839768@qq.com>
2020-08-28 15:03:45 -0500

    Merge PDF  files

--- MergePDF.py
+++ MergePDF.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8*-
-# 利用PyPDF2模块合并同一文件夹下的所有PDF文件
-# 只需修改存放PDF文件的文件夹变量：file_dir 和 输出文件名变量: outfile
+# 利用PyPDF2模块合并同一文件夹下的所有PDF文件，按文件索引数字顺序合并。
+# 存放PDF文件的文件夹名字即输出文件名。
+# 命令行参数，文件夹地址
 import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import time
-
+import sys
 # 使用os模块的walk函数，搜索出指定目录下的全部PDF文件
 # 获取同一目录下的所有PDF文件的绝对路径
 def getFileName(filedir):
@@ -24,39 +25,34 @@ def MergePDF(filepath, outfile):
     pdf_fileName = getFileName(filepath)
 
     if pdf_fileName:
-        pdf_fileName=sorted(pdf_fileName,key=lambda T:T.replace("/Users/gyr/Desktop/回国证明材料/","")[0])
+        pdf_fileName=sorted(pdf_fileName,key=lambda T:T.split("/")[-1])
         for pdf_file in pdf_fileName:
-            print("路径：%s"%pdf_file)
-
+            print("Path：%s"%pdf_file)
             # 读取源PDF文件
             input = PdfFileReader(open(pdf_file, "rb"))
-
             # 获得源PDF文件中页面总数
             pageCount = input.getNumPages()
             outputPages += pageCount
-            print("页数：%d"%pageCount)
-
+            print("Page：%d"%pageCount)
             # 分别将page添加到输出output中
             for iPage in range(pageCount):
                 output.addPage(input.getPage(iPage))
-
-        print("合并后的总页数:%d."%outputPages)
+                
+        print("Total Pages:%d."%outputPages)
         # 写入到目标PDF文件
         outputStream = open(os.path.join(filepath, outfile), "wb")
         output.write(outputStream)
         outputStream.close()
-        print("PDF文件合并完成！")
+        print("Finished！")
 
     else:
-        print("没有可以合并的PDF文件！")
+        print("Found no files！")
 
-# 主函数
-def main():
+
+if __name__=="__main__":
     time1 = time.time()
-    file_dir = r'/Users/gyr/Desktop/回国证明材料' # 存放PDF的原文件夹
-    outfile = "E48737838.pdf" # 输出的PDF文件的名称
+    file_dir = sys.argv[1]
+    outfile = file_dir.split("/")[-1]+".pdf"
     MergePDF(file_dir, outfile)
     time2 = time.time()
-    print('总共耗时：%s s.' %(time2 - time1))
-
-main()
+    print('Cost time：%s s.' %(time2 - time1))
